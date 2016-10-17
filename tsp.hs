52216a36c3ab22d14b932aae28dbe91c8f1d9614
marloutoyou <marloutoyou@hotmail.com>
2016-10-17 22:53:29 +0200

    progress on path function

--- tsp.hs
+++ tsp.hs
@@ -19,17 +19,22 @@
 
 import Data.List
 
-type City = (Float, Float)
+type City = (Int, Int)
 type Route = [City]
 type Population = [Route]
-type Map = [City] 
+type Map = [City]
+type Path = [(Int, Int)] 
+
+--while :: (a -> Bool) -> (a -> a) -> a -> a
+--while = until . (not.)
 
 dim :: Int
 dim = 20
 
 -- variabelen: population_size
 
-
+dumMap :: Map
+dumMap = [(1,5),(20,20),(6,4),(7,15),(12,4)]
 
 -- 3 functies? InitialPopulation, NextGeneration, SearchPopulation
 -- SearchPopulation : blijft de volgende generatie berekenen tot aan eis is voldaan of terminatie conditie bereikt
@@ -47,17 +52,31 @@ dim = 20
 -- bepalen: dimensies van kaart en hoe weer te geven?
 -- functie displayRoute
 
-displayRow :: [Int] -> IO()
-displayRow cities = 
-	let str = foldl (\y x -> if elem x cities then y ++ "x" else y ++ "-") "" [0..20]
-	in do putStrLn str
+displayRow :: Map -> Int -> String
+displayRow cities row = foldl (\y x -> if elem (row,x) cities then y ++ "x " else y ++ "- ") "" [0..dim]
 
 
+displayMap :: Map -> Int -> IO()
+displayMap _ 0 = return ()
+displayMap cities dim = putStrLn (displayRow cities dim) >> displayMap cities (dim - 1)
 
-displayMap :: IO()
-displayMap = do
-	putStrLn "--+---------+---x-----*---"
-	putStrLn "+------+----------x----*--"
+
+findPath :: Route -> Path
+findPath [] = []
+findPath cities = 
+	let city1 = head cities
+		city2 = head (tail cities)
+		slope = case (fst city1 - fst city2) of
+			0 -> 100000
+			_ -> (snd city1 - snd city2)/(fst city1 - fst city2)
+		b = (snd city1) - slope*(fst city1)
+		-- function is now slope*x + b
+		if fst city1 < city2
+			then leftCity = city1
+				 rightCity = city2
+			else leftCity =  city2
+				 rightCity = city1
+		in ()
 
 
 --displayRoute :: Route -> IO()
