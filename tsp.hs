77b43c4a4ae3b32782ae751b5d02a45f0e1de044
marloutoyou <marloutoyou@hotmail.com>
2016-10-19 18:30:37 +0200

    begonnen aan standaard benodigdheden voor de GA

--- tsp.hs
+++ tsp.hs
@@ -1,3 +1,7 @@
+----------------------------------
+-- NOTES
+---------------------------------
+
 -- ook zorgen dat t goed wordt weergegeven.
 -- mogelijke volgorde van dingen doen:
 -- eerst initial population (LET OP DAT NIET 2 DEZELFDE COORDINATEN MOGELIJK!!) en search population doen, en een dummie next generation (jatten?)
@@ -17,24 +21,8 @@
 -- kan wel zoeken naar opeenvolgende steden die in de route erg dicht bij elkaar zitten
 -- je kan een soort gemiddelde afstand berekenen en dan kijken wat daar (ver?) onder ligt
 
-import Data.List
 
-type City = (Integer, Integer)
-type Route = [City]
-type Population = [Route]
-type Map = [City]
-type Path = [(Integer, Integer)] 
 
---while :: (a -> Bool) -> (a -> a) -> a -> a
---while = until . (not.)
-
-dim :: Integer
-dim = 20
-
--- variabelen: population_size
-
-dumMap :: Map
-dumMap = [(1,5),(20,20),(6,4),(7,15),(12,4)]
 
 -- 3 functies? InitialPopulation, NextGeneration, SearchPopulation
 -- SearchPopulation : blijft de volgende generatie berekenen tot aan eis is voldaan of terminatie conditie bereikt
@@ -52,26 +40,77 @@ dumMap = [(1,5),(20,20),(6,4),(7,15),(12,4)]
 -- bepalen: dimensies van kaart en hoe weer te geven?
 -- functie displayRoute
 
+----------------------------------------------------------------------------------------------------------------------
+
+
+
+---------------------------------------------
+-- Imports and types and starter variables
+---------------------------------------------
+
+import Data.List
+import GA
+
+type City = (Integer, Integer)
+type Route = [City] -- gives the cities in order of the route
+type Population = [Route]
+type Map = [City] -- not necessarily a route, just the coordinates of the cities
+type Path = [(Integer, Integer)] -- for the coordinates of the paths between the cities
+
+--while :: (a -> Bool) -> (a -> a) -> a -> a
+--while = until . (not.)
+
+dim :: Integer
+dim = 20
+
+big :: Float
+big = 100000
+
+-- variabelen: population_size
+
+dumMap :: Map
+dumMap = [(1,5),(20,20),(6,4),(7,15),(12,4)]
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+
+------------------------------
+-- STUFF FOR DISPLAYING
+------------------------------
+
+------------------------------------------------------------------
+-- for displaying a map with cities
+------------------------------------------------------------------
+
+-- helper for displayMap
+-- display the row for a map. Cities are represented by an x 
 displayRowMap :: Map -> Integer -> String
 displayRowMap cities row = foldl (\y x -> if elem (row,x) cities then y ++ "x " else y ++ "- ") "" [0..dim]
 
-
+-- display the map with cities
 displayMap :: Map -> Integer -> IO()
 displayMap _ (-1) = return ()
 displayMap cities dim = putStrLn (displayRowMap cities dim) >> displayMap cities (dim - 1)
 
 
--- slope == 100000
+---------------------------------------------------------------------
+-- functions for calculating the points on a path between two cities
+---------------------------------------------------------------------
+
+-- First we calculate the line between two points. 
+-- Since we cannot display that with IO, the points are rounded as integers and displayed with a * on the map
+
+-- if the cities are above each other (so the line between them is represented by the function x = a for some a)
 calcVertical :: City -> City -> Path
 calcVertical city1 city2 =
   let x = fst city1
       (hcity, lcity) = case (snd city1 > snd city2) of
         True -> (city1, city2)
         False -> (city2, city1)
-	in foldl (\a b -> (x,b):a) [] [snd lcity + 1.. snd hcity - 1]
+  in foldl (\a b -> (x,b):a) [] [snd lcity + 1.. snd hcity - 1]
 
 
--- slope > 1
+-- if the slope > 1 for the slope of the line between the two points
 calcViaY :: City -> City -> Float -> Float -> Path
 calcViaY city1 city2 slope b =
 	let (hcity, lcity) = case (snd city1 > snd city2) of
@@ -79,7 +118,7 @@ calcViaY city1 city2 slope b =
 			False -> (city2, city1)
 	in foldl (\list y -> (round ((fromInteger y - b)/slope),y):list) [] [snd lcity + 1.. snd hcity - 1]
 
--- rest
+-- rest (if the slope <= 1 and they are not directly above each other)
 calcViaX :: City -> City -> Float -> Float -> Path
 calcViaX city1 city2 slope b =
 	let (lcity, rcity) = case (fst city1 > fst city2) of
@@ -88,32 +127,87 @@ calcViaX city1 city2 slope b =
 	in foldl (\list x -> (x, round (slope*(fromInteger x) + b)):list) [] [fst lcity + 1 .. fst rcity - 1]
 
 
--- zorgen dat de 1e en laatste ook geconnect worden
+-- calculates the slope and the "b" of a line between two coordinates
+-- y = slope*x + b
+findLine :: City -> City -> (Float, Float)
+findLine city1 city2 = 
+  let city1x = fromInteger (fst city1)
+      city1y = fromInteger (snd city1)
+      city2x = fromInteger (fst city2)
+      city2y = fromInteger (snd city2)
+      slope = case (city1x - city2x) of
+          0 -> big
+          _ -> (city1y - city2y)/(city1x - city2x)
+      b = city1y - slope*city1x
+  in (slope, b)
+
+-- finds all the points of the paths between the cities in a route. 
+-- this function requires that the first and the last element of the route are equal (in order to make a round trip)
+-- it takes the first two elements of route and finds the points of the path between them
 findPath :: Route -> Path
 findPath [a] = []
 findPath route = 
+  let city1 = head route
+      city2 = head (tail route)
+      (slope, b) = findLine city1 city2
+      rest = tail route
+      in if slope == big
+        then calcVertical city1 city2 ++ findPath rest
+        else if slope > 1
+          then calcViaY city1 city2 slope b ++ findPath rest
+          else calcViaX city1 city2 slope b ++ findPath rest
+
+------------------------------------------------------------------
+-- for displaying a route 
+------------------------------------------------------------------
+
+-- helper function for displayRoute
+-- displaying the row of a route. Cities are an x, points of the path are a *
+displayRowRoute :: Route -> Path -> Integer -> String
+displayRowRoute route path row = foldl (\y x -> if elem (row,x) route then y ++ "x " else if elem (row,x) path then y ++ "* " else y ++ "- ") "" [0..dim]
+
+-- display a route!
+displayRoute :: Route -> Path -> Integer -> IO()
+displayRoute _ _ (-1) = return ()
+displayRoute route path dim = putStrLn (displayRowRoute route path dim) >> displayRoute route path (dim - 1)
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------
+-- GENERAL STUFF FOR THE GA ALGORITHM
+-----------------------------------------
+
+-- generating the pool:
+allCoordinates :: [City]
+allCoordinates = [(x,y) | x <- [0..dim], y <- [0..dim]]
+
+-- fitness:
+-- calculate length of route, returned as a float
+-- requires that the first and the last element of route are equal (to make a circle)
+-- NOG TESTEN
+calcLength :: Route -> Float
+calcLength [a] = 0
+calcLength route = 
   let city1 = head route
       city2 = head (tail route)
       city1x = fromInteger (fst city1)
       city1y = fromInteger (snd city1)
       city2x = fromInteger (fst city2)
       city2y = fromInteger (snd city2)
-      slope = case (city1x - city2x) of
-          0 -> 100000
-          _ -> (city1y - city2y)/(city1x - city2x)
-      b = city1y - slope*city1x
-      -- function is now slope*x + b
+      dy = abs (city1y - city2y)
+      dx = abs (city1x - city1y)
+      distance = sqrt (dx^2 + dy^2)
       rest = tail route
-      in if slope == 100000
-        then calcVertical city1 city2 ++ findPath rest
-        else if slope > 1
-          then calcViaY city1 city2 slope b ++ findPath rest
-          else calcViaX city1 city2 slope b ++ findPath rest
+  in distance + calcLength rest
 
+-----------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------
+-- REQUIRED FUNCTIONS
+--------------------------------------------
 
-displayRowRoute :: Route -> Path -> Integer -> String
-displayRowRoute route path row = foldl (\y x -> if elem (row,x) route then y ++ "x " else if elem (row,x) path then y ++ "* " else y ++ "- ") "" [0..dim]
+-- copied from the package: 
+-- Minimal implementation should include 'genRandom', 'crossover', 'mutation', 
+-- and either 'score'', 'score' or 'scorePop'.
 
-displayRoute :: Route -> Path -> Integer -> IO()
-displayRoute _ _ (-1) = return ()
-displayRoute route path dim = putStrLn (displayRowRoute route path dim) >> displayRoute route path (dim - 1)
+-- score' function as in package
+score' _ route = Just (calcLength route)
+
