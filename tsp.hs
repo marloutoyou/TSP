57595c17cf82633528dee8c280062931a0ea53e7
marloutoyou <marloutoyou@hotmail.com>
2016-10-28 16:56:21 +0200

    adjaslkd

--- tsp.hs
+++ tsp.hs
@@ -48,11 +48,15 @@ noCity = (-1, -1)
 
 -- the dimension of the map
 dim :: Integer
-dim = 20
+dim = 1750
 
 -- the map used as a pool
 dummyMap :: Map
 dummyMap = [(1,1),(11,1),(19,1),(19,9),(19,19),(10,19),(1,19),(1,3)]
+
+-- dimension: 1750
+berlin52 :: Map
+berlin52 = [(565, 575),(25, 185), (345, 750), (945, 685), (845, 655), (880, 660), (25, 230), (525, 1000), (580, 1175), (650, 1130), (1605, 620), (1220, 580), (1465, 200), (1530, 5), (845, 680), (725, 370), (145, 665), (415, 635), (510, 875), (560, 365), (300, 465), (520, 585), (480, 415), (835, 625), (975, 580), (1215, 245), (1320, 315), (1250, 400), (660, 180), (410, 250), (420, 555), (575, 665), (1150, 1160), (700, 580), (685, 595), (685, 610), (770, 610), (795, 645), (720, 635), (760, 650), (475, 960), (95, 260), (875, 920), (700, 500), (555, 815), (830, 485), (1170, 65), (830, 610), (605, 625), (595, 360), (1340, 725), (1740, 245)]
 
 
 -----------------------------------------------------------------------------------------------------------------------------
@@ -435,11 +439,11 @@ instance Entity Route Float () Map IO where
 
   score' = score''
 
-  mutation = selectiveMutation
+  mutation = tworsMutation
 
   crossover = selectiveCrossover
 
-  showGeneration = showRoute
+  -- showGeneration = showRoute
 
 
 main :: IO()
@@ -447,7 +451,7 @@ main = do
         let cfg = GAConfig 
                     200 -- population size
                     25 -- archive size (best entities to keep track of)
-                    5 -- maximum number of generations
+                    100 -- maximum number of generations
                     0.8 -- crossover rate (% of entities by crossover)
                     0.2 -- mutation rate (% of entities by mutation)
                     0.0 -- parameter for crossover (not used here)
@@ -456,7 +460,7 @@ main = do
                     False -- don't rescore archive in each generation
 
             g = mkStdGen 0 -- random generator
-            pool = dummyMap
+            pool = berlin52
 
         es <- evolveVerbose g cfg pool ()
         let e = snd $ head es :: Route
@@ -468,10 +472,10 @@ main = do
 
         -- Compare with random search with large budget
         -- 100k random entities, equivalent to 1000 generations of GA
-        es' <- randomSearch g 100000 pool ()
-        let e' = snd $ head es' :: Route
-        let eLen' = calcTotalLength (makeRoundTrip e')
+        --es' <- randomSearch g 100000 pool ()
+        --let e' = snd $ head es' :: Route
+        --let eLen' = calcTotalLength (makeRoundTrip e')
        
-        putStrLn $ "best entity (random search): " ++ show e'
-        putStrLn $ "length: " ++ show eLen'
+        --putStrLn $ "best entity (random search): " ++ show e'
+        --putStrLn $ "length: " ++ show eLen'
 
